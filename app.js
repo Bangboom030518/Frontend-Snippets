c2193fedbe492f313b7f4230a86ddbc29368652c
Charlie Clift <bang.boom030518@gmail.com>
2023-09-29 20:38:24 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+    var email = $("#email").val();
+    if (validateEmail(email)) {
+        alert("valid!")
+    } else {
+        alert("NOT VALID")
+    }
+    return false;
+  }
+  
+  $("#validate").on("click", validate);
+  
